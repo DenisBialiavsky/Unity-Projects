7d7cbfff602f2460b5cd8437eb3f8a08c118d44f
DenisBialiavsky <denis.bialiauski@gmail.com>
2020-10-06 16:56:49 +0300

    Unity project

---
+++ My try too/Assets/BackAndForth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForth : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+    public float maxZ = 16.0f; //<- Объект движется между этими точками.
+    public float minZ = -16.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+    private int _direction = 1; //<- В каком направлении объект движется в данный момент?
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+        bool bounced = false;
+        if (transform.position.z > maxZ || transform.position.z < minZ)
+        {
+            _direction = -_direction; //<- Меняем направление на противоположное.
+            bounced = true;
+        }
+        if (bounced)
+        { //<- Делаем дополнительное движение в этом кадре, если объект поменял направление.
+            transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ My try too/Assets/WanderingAI.cs
@@ -0,0 +1,97 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{
+
+    [SerializeField] private GameObject fireballPrefab; //<- Эти два поля добавляются
+    // перед любыми методами, как и в сценарии SceneController
+    private GameObject _fireball;
+    public  float baseSpeed = 3.0f;// - Базовая скорость, меняемая 
+                                        //в соответствии с положением ползунка
+
+    public float speed = 3.0f; //<- Значения для скорости движения и расстояния
+    //с которого начинается реакция на препятствие
+    public float obstacleRange = 5.0f;
+
+    private bool _alive;//<-Логическая переменная для слежения за состоянием персонажа.
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _alive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_alive)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime); //<- Непрерывно движемся
+                                                               //вперед в каждом кадре, несмотря на повороты
+        }
+
+
+
+
+        Ray ray = new Ray(transform.position, transform.forward); //<- Луч находится
+        // в том же положении и нацеливается в том же направлении, что и персонаж
+
+        RaycastHit hit;
+
+        //if (Physics.SphereCast(ray, 0.75f, out hit))
+        //{//<-Бросаем луч с описанной вокруг него окружностью
+        //    if (hit.distance < obstacleRange)
+        //    {
+        //        float angle = Random.Range(-110, 110); //<- Поворот с наполовину
+        //        // случайным выбором нового направления.
+        //        transform.Rotate(0, angle, 0);
+        //    }
+        //}
+
+        if (Physics.SphereCast(ray, 0.75f, out hit))
+        {
+            GameObject hitObject = hit.transform.gameObject;
+            if (hitObject.GetComponent<PlayerCharacter>())
+            {//<- Игрок распознается тем же спсобом, что и мишень в сценарии RayShooter
+                if (_fireball == null)
+                { //<- Та же самая логика с пустым игровым объектом, что и в сценарии SceneController.
+                    _fireball = Instantiate(fireballPrefab) as GameObject; //Метод
+                    // Instantiate() работает так же, как и в сценарии SceneController.
+                    _fireball.transform.position = //<- Поместим огненный шар в перед врагом
+                                              // и нацелим в направлении его движения
+                    transform.TransformPoint(Vector3.forward * 1.5f);
+                    _fireball.transform.rotation = transform.rotation;
+                }
+            }
+            else if (hit.distance < obstacleRange)
+            {
+                float angle = Random.Range(-110, 110);
+                transform.Rotate(0, angle, 0);
+
+            }
+        }
+    }
+    public void SetAlive(bool live)//<-Открытый метод, позволяющий внешнему коду
+        // воздействовать на "живое" состояние.
+    {
+        _alive = live;
+    }
+   
+    void Awake()
+    {
+        Messenger<float>.AddListener(GameEvent.SPEED_CHANGED, OnSpeedChanged);
+    }
+    void OnDestroy()
+    {
+        Messenger<float>.RemoveListener(GameEvent.SPEED_CHANGED, OnSpeedChanged);
+    }
+    private void OnSpeedChanged(float value)//<- Метод, объявленный в подписчике для 
+                                            //события SPEED CHANGED
+    {
+        speed = baseSpeed * value;
+    }
+}
